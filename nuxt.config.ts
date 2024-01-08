df02037ebad51aa379a07bd1a836229b1caa1ecc
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2024-01-08 08:55:28 +0100

    feat(sass) : import du fichier variables dans nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  css:['@/assets/main.scss']
+  css:['@/assets/main.scss'],
+
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: 
+          `@import "@/assets/foundations/variables.scss";`
+        }
+      }
+    }
+    
+  },
 })
